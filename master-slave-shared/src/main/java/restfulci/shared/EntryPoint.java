c2831910fb51ad8d4085f7c28373aef778af7f7d
Cong-Xin Qiu <ozoox.o@gmail.com>
2020-02-25 18:46:21 -0500

    Comments and TODOs

--- master-slave-shared/src/main/java/restfulci/shared/EntryPoint.java
+++ master-slave-shared/src/main/java/restfulci/shared/EntryPoint.java
@@ -3,6 +3,15 @@ package restfulci.shared;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * TODO:
+ * Cannot move dao classes to shared, otherwise master-api errors out 
+ * that CrubRepository DAO cannot be autowired.
+ * > Field jobRepository in restfulci.master.service.JobServiceImpl required 
+ * > a bean of type 'restfulci.shared.dao.JobRepository' that could not be found.
+ * 
+ * Seems `@ComponentScan` doesn't help either. 
+ */
 @Configuration
 @EntityScan(basePackages = {"restfulci.shared"})
 public class EntryPoint {
--- slave-agent/src/main/java/restfulci/slave/SlaveApplication.java
+++ slave-agent/src/main/java/restfulci/slave/SlaveApplication.java
@@ -5,7 +5,10 @@ import java.util.function.Consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @SpringBootApplication(scanBasePackages= {"restfulci.slave", "restfulci.shared"})
 public class SlaveApplication {
@@ -20,4 +23,29 @@ public class SlaveApplication {
 			System.out.println("Received: " + input);
 		};
 	}
+	
+	/*
+	 * TODO:
+	 * 
+	 * Cannot easily do this, because `RunBean` has subclasses, and it seems
+	 * the JSON parser cannot successfully build subclasses. 
+	 * 
+	 * That's maybe fine/not needed, as we should probably define special DTO
+	 * for communicating between master and slave, rather than use the exiting
+	 * `RunBean`.
+	 */
+//	@Bean
+//	public Consumer<RunBean> executeRun() {
+//		return input -> {
+//			System.out.println("Received job name: " + input.getJob().getName());
+//			
+//			ObjectMapper objectMapper = new ObjectMapper();
+//			ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+//			try {
+//				System.out.println("Received JSON: " + objectWriter.writeValueAsString(input));
+//			} catch (JsonProcessingException e) {
+//				
+//			}
+//		};
+//	}
 }
