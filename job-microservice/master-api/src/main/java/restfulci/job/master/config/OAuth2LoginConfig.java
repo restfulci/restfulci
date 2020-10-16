dcb7b05e6f516dc2ada37f66f750617072cb7cf4
Cong-Xin Qiu <ozoox.o@gmail.com>
2020-10-16 01:38:17 -0400

    JWT cleanup

--- job-microservice/master-api/src/main/java/restfulci/job/master/config/OAuth2LoginConfig.java
+++ job-microservice/master-api/src/main/java/restfulci/job/master/config/OAuth2LoginConfig.java
@@ -13,41 +13,38 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
  * https://github.com/spring-projects/spring-security/tree/master/samples/boot/oauth2resourceserver-jwe
  */
 @Configuration
-public class OAuth2LoginConfig {
-	
-	@EnableWebSecurity
-	public static class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class OAuth2LoginConfig extends WebSecurityConfigurerAdapter {
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-				.authorizeRequests(authorize -> authorize
-//						.antMatchers("/", "/error").permitAll()
-						.anyRequest().authenticated()
-						)
-				.csrf().disable()
-				.oauth2ResourceServer(oauth2 -> oauth2
-						.jwt(jwt -> jwt
-								.decoder(jwtDecoder())
-								)
-						);
-		}
-		
-		public JwtDecoder jwtDecoder() {
-			NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder
-		    		.withJwkSetUri("http://localhost:8880/auth/realms/restfulci/protocol/openid-connect/certs")
-		    		.build();
-			jwtDecoder.setJwtValidator(
-					JwtValidators.createDefaultWithIssuer("http://localhost:8880/auth/realms/restfulci"));
-			return jwtDecoder;
-		}
-		
-		/*
-		 * Token generated from calling Keycloak endpoint `/e2e-api/test_auth.sh`.
-		 * 
-		 * TODO/questions:
-		 * Why this application doesn't need to know clientId/secretId,
-		 * but only the query to get the token need to know it?
-		 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(authorize -> authorize
+//					.antMatchers("/", "/error").permitAll()
+					.anyRequest().authenticated()
+					)
+			.csrf().disable()
+			.oauth2ResourceServer(oauth2 -> oauth2
+					.jwt(jwt -> jwt
+							.decoder(jwtDecoder())
+							)
+					);
 	}
+	
+	public JwtDecoder jwtDecoder() {
+		NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder
+				.withJwkSetUri("http://localhost:8880/auth/realms/restfulci/protocol/openid-connect/certs")
+				.build();
+		jwtDecoder.setJwtValidator(
+				JwtValidators.createDefaultWithIssuer("http://localhost:8880/auth/realms/restfulci"));
+		return jwtDecoder;
+	}
+	
+	/*
+	 * Token generated from calling Keycloak endpoint `/e2e-api/test_auth.sh`.
+	 * 
+	 * TODO/questions:
+	 * Why this application doesn't need to know clientId/secretId,
+	 * but only the query to get the token need to know it?
+	 */
 }
