2fb39cc12aa2d9da9db05d34e6bf79f0bd74839e
Cong-Xin Qiu <ozoox.o@gmail.com>
2021-02-13 08:41:36 -0500

    Zuul gateway hello world

---
+++ api-gateway/src/main/java/restfulci/gateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package restfulci.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import restfulci.gateway.filters.pre.SimpleFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+		public SimpleFilter simpleFilter() {
+			return new SimpleFilter();
+		}
+}
