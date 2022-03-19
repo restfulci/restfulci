8d55b5032a2ab9cfe01bf02bebe0f122b045b0ed
Cong-Xin Qiu <ozoox.o@gmail.com>
2022-03-19 10:42:56 -0400

    Fix GitCloneTest after master branch renaming

--- job-microservice/master-slave-shared/src/test/java/restfulci/job/shared/dao/git/GitCloneTest.java
+++ job-microservice/master-slave-shared/src/test/java/restfulci/job/shared/dao/git/GitCloneTest.java
@@ -24,41 +24,41 @@ public class GitCloneTest {
 	@Test
 	@DisabledIfEnvironmentVariable(named="CI", matches="CircleCI")
 	public void testGitCloneFromLocal(@TempDir File tmpFolder) throws Exception {
-		
+
 		File sourceDirectory = new File(tmpFolder, "source-repo");
 		sourceDirectory.mkdir();
-		
+
 		ProcessBuilder builder = new ProcessBuilder("git", "init");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		builder = new ProcessBuilder("touch", "a-file.txt");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		builder = new ProcessBuilder("git", "add", "-A");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		builder = new ProcessBuilder("git", "commit", "-m", "\"add a-file.txt\"");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		builder = new ProcessBuilder("touch", "another-file.txt");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		builder = new ProcessBuilder("git", "add", "-A");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		builder = new ProcessBuilder("git", "commit", "-m", "\"add another-file.txt\"");
 		builder.directory(sourceDirectory);
 		builder.start().waitFor();
-		
+
 		File localDirectory = new File(tmpFolder, "local-repo");
 		localDirectory.mkdir();
-		
+
 		GitClone gitClone = new GitClone("file://"+sourceDirectory.getAbsolutePath(), localDirectory);
 		gitClone.setBranchName("master");
 		gitClone.setDepth(1);
@@ -67,7 +67,7 @@ public class GitCloneTest {
 		assertTrue(gitResult.getNormalOutput().isEmpty());
 		assertTrue(gitResult.getErrorOutput().startsWith("Cloning into"));
 		assertEquals(gitResult.getExitCode(), 0);
-		
+
 		List<String> lsArray = new ArrayList<String>();
 		lsArray.add("/bin/sh");
 		lsArray.add("-c");
@@ -75,22 +75,22 @@ public class GitCloneTest {
 		CommandResult lsResult = Executable.executeWith(lsArray, localDirectory);
 		assertEquals(lsResult.getNormalOutput(), "a-file.txt\nanother-file.txt\n");
 	}
-	
+
 	@Test
 	public void testGitCloneFromGitHubHttpsProtocol(@TempDir File tmpFolder) throws Exception {
-		
+
 		File localDirectory = new File(tmpFolder, "local-repo");
 		localDirectory.mkdir();
-		
+
 		GitClone gitClone = new GitClone("https://github.com/restfulci/restfulci.git", localDirectory);
-		gitClone.setBranchName("master");
+		gitClone.setBranchName("main");
 		gitClone.setDepth(1);
 		CommandResult gitResult = gitClone.execute();
 		assertTrue(gitResult.getCommand().startsWith("git clone"));
 		assertTrue(gitResult.getNormalOutput().isEmpty());
 		assertTrue(gitResult.getErrorOutput().startsWith("Cloning into"));
 		assertEquals(gitResult.getExitCode(), 0);
-		
+
 		List<String> lsArray = new ArrayList<String>();
 		lsArray.add("/bin/sh");
 		lsArray.add("-c");
@@ -98,22 +98,22 @@ public class GitCloneTest {
 		CommandResult lsResult = Executable.executeWith(lsArray, localDirectory);
 		assertTrue(lsResult.getNormalOutput().contains("README.md"));
 	}
-	
+
 	@Test
 	public void testGitCloneFromGitHubSshProtocol(@TempDir File tmpFolder) throws Exception {
-		
+
 		File localDirectory = new File(tmpFolder, "local-repo");
 		localDirectory.mkdir();
-		
+
 		GitClone gitClone = new GitClone("git@github.com:restfulci/restfulci.git", localDirectory);
-		gitClone.setBranchName("master");
+		gitClone.setBranchName("main");
 		gitClone.setDepth(1);
 		CommandResult gitResult = gitClone.execute();
 		assertTrue(gitResult.getCommand().startsWith("git clone"));
 		assertTrue(gitResult.getNormalOutput().isEmpty());
 		assertTrue(gitResult.getErrorOutput().startsWith("Cloning into"));
 		assertEquals(gitResult.getExitCode(), 0);
-		
+
 		List<String> lsArray = new ArrayList<String>();
 		lsArray.add("/bin/sh");
 		lsArray.add("-c");
@@ -121,32 +121,32 @@ public class GitCloneTest {
 		CommandResult lsResult = Executable.executeWith(lsArray, localDirectory);
 		assertTrue(lsResult.getNormalOutput().contains("README.md"));
 	}
-	
+
 	@Test
 	public void testGitCloneNonexistRepo(@TempDir File tmpFolder) throws Exception {
-		
+
 		File localDirectory = new File(tmpFolder, "local-repo");
 		localDirectory.mkdir();
-		
+
 		GitClone gitClone = new GitClone("git@github.com:restfulci/nonexist.git", localDirectory);
-		gitClone.setBranchName("master");
+		gitClone.setBranchName("main");
 		gitClone.setDepth(1);
-		
+
 		assertThrows(GitRepoNonExistException.class, () -> {
 			gitClone.execute();
 		});
 	}
-	
+
 	@Test
 	public void testGitCloneNonExistBranch(@TempDir File tmpFolder) throws Exception {
-		
+
 		File localDirectory = new File(tmpFolder, "local-repo");
 		localDirectory.mkdir();
-		
+
 		GitClone gitClone = new GitClone("git@github.com:restfulci/restfulci.git", localDirectory);
 		gitClone.setBranchName("nonexist");
 		gitClone.setDepth(1);
-		
+
 		assertThrows(GitBranchNonExistException.class, () -> {
 			gitClone.execute();
 		});
