4a3619bf9aebc491ce468c4dfecdfc5b70a5d933
Cong-Xin Qiu <ozoox.o@gmail.com>
2022-03-23 21:56:32 -0400

    Setup api-six

--- api-gateway/docker_cleanup.sh
+++ api-gateway/docker_cleanup.sh
@@ -1,4 +1,7 @@
-docker-compose rm -v -f keycloak-postgres
-docker-compose rm -v -f job-postgres
-docker-compose rm -v -f job-rabbitmq
-docker-compose rm -v -f pipeline-postgres
+docker system prune
+docker system prune --volumes
+
+docker-compose -f docker-compose.upstream.yml rm -v -f keycloak-postgres
+docker-compose -f docker-compose.upstream.yml rm -v -f job-postgres
+docker-compose -f docker-compose.upstream.yml rm -v -f job-rabbitmq
+docker-compose -f docker-compose.upstream.yml rm -v -f pipeline-postgres
